d6e6f1ad1810a40e4fce8732043bcf481f0cb8d8
LeonardoAValerio <leonardoaugustovalerio@gmail.com>
2024-09-20 18:39:05 -0300

    First commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const db = require('./db');
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  try {
+    const result = await db.query('SELECT * FROM book');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
