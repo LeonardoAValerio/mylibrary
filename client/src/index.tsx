b4bc3559e31a44d639f071a2c44b5126964b5e6c
LeonardoAValerio <leonardoaugustovalerio@gmail.com>
2024-10-06 21:01:37 -0300

    feat(client): create partial home page

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRoutes from './routes';
+import "./index.css"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,37 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Book } from "../../components/Book"
+import './home.css'
 
-interface BookAttibrutes {
-  title: string;
+export interface BookAttributes {
   id: string;
+  title: string;
+  synopse?: string;
+  review?: string;
+  urlImage?: string;
 }
 
 export function Home() {
-  const [books, setBooks] = useState<BookAttibrutes[]>([]);
+  const [books, setBooks] = useState<BookAttributes[]>([]);
 
   useEffect(() => {
     const fetchBooks = async () => {
       try {
-        const response = await axios.get('http://localhost:8081/books'); 
+        const response = await axios.get('http://localhost:8081/books');
         setBooks(response.data);
       } catch (error) {
 
-      } 
+      }
     };
 
     fetchBooks();
   }, []);
 
   return (
-    <div>
+    <main className="home">
       <h1>Livros:</h1>
-      <div>
-        {books.map((book) => (
-          <li key={book.id}>
-            <strong>{book.title}</strong>
-          </li>
-        ))}
+      <div className='align-library'>
+        <div className="library">
+          {books.map((book) => (
+            <Book id={book.id} title={book.title} urlImage={book.urlImage}></Book>
+          ))}
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
