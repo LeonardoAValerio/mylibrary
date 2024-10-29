fc112f757286f94614107b978f6c4b8f2b0c56ca
LeonardoAValerio <leonardoaugustovalerio@gmail.com>
2024-10-29 00:02:37 -0300

    feat: create autheticatorRoute to loginToken

---
+++ server/src/middleware/authRoute.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from 'jsonwebtoken';
+import enviorments from "../enviorments";
+import { CustomError } from "../helpers/Errors";
+
+
+
+export function authTokenMiddleware(req: Request, res: Response, next: NextFunction) {
+    try {
+        const authToken = req.headers.authtoken as string;
+    
+        if (!authToken) {
+            res.status(403).send({ message: "Invalid authToken!" });
+        }
+
+        jwt.verify(authToken, enviorments.KEY_JWT, (err) => {
+            if(err) throw new CustomError("Invalid token!")
+        });
+
+        next();
+    } catch (err) {
+        res.status(403).send({ message: "Invalid authToken!" });
+    }
+}
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import bookController from "./controllers/Books";
 import loginController from "./controllers/Login";
+import { authTokenMiddleware } from "./middleware/authRoute";
 
 const routes = Router();
 
-routes.use("/books", bookController);
+routes.use("/books", authTokenMiddleware, bookController);
 routes.use("/login", loginController)
 
 export default routes;
